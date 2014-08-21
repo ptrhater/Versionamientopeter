38a6edf5c259934b5b9e597200f3ab9d0bb1fdf9
rolando arriaza <rolignu90@gmail.com>
2014-08-21 12:42:25 -0600

    Clase ARRAYSORT
    te anexo la clase de arraysort y el ejercicio

---
+++ leccion ArraySORT/index.php
@@ -0,0 +1,48 @@
+
+
+<?php
+
+//SORT: SON FUNCIONES DE ORDENAMIENTO QUE PHP TRAE POR DEFECTO 
+/**
+ * 
+ * LOS TIPOS DE SORT MAS FRECUENTE SON 
+ * 
+ * sort(array()) -> devuelve un arreglo en orden ascendente
+ * rsort(array()) -> devuelve un arreglo en orden descendente
+ * asort(array()) -> devuelve un arreglo en orden ascendente deacuerdo al "value" 
+ * ejemplo array("roli"=>"guapo"); el value es "guapo"
+ * arsort(array()) ->los mismos patrones solo que de forma descendente
+ * 
+ */
+
+
+//sort simple devuelve de forma ascendente marcas de carros
+$cars=array("Volvo","BMW","Toyota");
+sort($cars);
+
+echo "<PRE>";
+print_r($cars);
+echo "</PRE><br><br>";
+
+rsort($cars);
+echo "<PRE>";
+print_r($cars);
+echo "</PRE><br><br>";
+
+
+$arreglo = array( "numero"=> 10 , "codigo"=>5 , "nota" => 8);
+asort($arreglo);
+//ascendente
+echo "<PRE>";
+print_r($arreglo);
+echo "</PRE><br><br>";
+
+
+arsort($arreglo);
+//descendente
+echo "<PRE>";
+print_r($arreglo);
+echo "</PRE><br><br>";
+
+?>
+
