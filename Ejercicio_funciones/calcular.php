f761460c3bf4ad6db738f5b5c813f0eca4ad9018
Peter Cruz <im.petercruz@gmail.com>
2014-08-21 10:28:30 -0600

    Ejercicio_funciones
    ahi  esta lo hice y todo funciona pero no se si hay cosas de mas o puedo
    mejorarlo obviamente si xd Revisalo

---
+++ Ejercicio_funciones/calcular.php
@@ -0,0 +1,47 @@
+<?php
+ function operaciones($n1 , $n2 , $operacion)
+ {
+     $_GET['combo'] = $operacion;
+     $_GET['n1'] = $n1;
+     $_GET['n2'] = $n2;
+     $total = "";
+  
+             
+     if ($operacion == "suma")
+     {
+         $total = $n1 + $n2;   
+     }
+     elseif ($operacion == "resta")
+     {
+         $total = $n1 - $n2;
+     }
+     elseif ($operacion == "multiplicacion")
+     {
+         $total = $n1 * $n2;
+     }
+     elseif ($operacion == "division")
+     {
+         $total = $n1 / $n2;
+     }
+     
+     return $total;
+          
+ }
+ 
+ $a = operaciones($_GET['n1'], $_GET['n2'], $_GET['combo']);
+         echo $a ; 
+         
+     if ( $_GET['combo'] == "elevar")
+     {
+       echo pow($_GET['n1'],2);   
+     }
+     elseif ($_GET['combo'] == "raiz") 
+     {
+       echo sqrt($_GET['n1']);  
+     
+       
+     }
+     
+
+
+?>
---
+++ Ejercicio_funciones/index.php
@@ -0,0 +1,15 @@
+<html>
+    <form method="get" action="calcular.php">
+        <input type="text" name="n1" placeholder="Ingrese un numero">
+        <input type="text" name="n2" placeholder="Ingrese otro numero">        
+    <select name="combo">
+            <option value="suma">Suma</option>
+            <option value="resta">Resta</option>
+            <option value="multiplicacion">Multiplicacion</option>
+            <option value="division">Division</option>
+            <option value="elevar">Elevar</option>
+            <option value="raiz">Raiz</option>
+    </select><br><br>
+    <input type="submit" value="Calcular!">
+    </form>
+</html>
